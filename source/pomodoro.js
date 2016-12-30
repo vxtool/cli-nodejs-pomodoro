b3adc67c75f0c544bb925773e1c5393377bbd7cd
Hemerson Vianna <hemerson.lourenco@gmail.com>
2016-12-30 20:30:29 -0200

    [add] pomodoro script

---
+++ source/pomodoro.js
@@ -0,0 +1,24 @@
+const moment = require('moment');
+const pad = require('./utils');
+
+const Pomodoro = () => {
+  this.timerFrom = '';
+  this.timerTo = '';
+  this.message = '';
+}
+
+Pomodoro.prototype.setTimer = (time, duration) => {
+  this.timerFrom = moment.duration(0, 'seconds')
+  this.timerTo = moment.duration(time, duration);
+}
+
+Pomodoro.prototype.setMessage = message => this.message = message;
+Pomodoro.prototype.getTimerTo = () => this.timerTo;
+Pomodoro.prototype.getMessage = () => this.message;
+Pomodoro.prototype.getTime = (type) => {
+  return pad(this[type].minutes(),0,2)+':'+pad(this[type].seconds(),0,2);
+}
+Pomodoro.prototype.totalSeconds = () => this.timerTo.asSeconds()+1;
+Pomodoro.prototype.tick = () => this.timerFrom.add(1, 's');
+
+module.exports = new Pomodoro();
